3bc0ed8ddc01b670f5de9f0d8c50d2868d54ba6a
Gitesh Sharma <gitesh15796@gmail.com>
2024-04-20 22:28:41 +0530

    home modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,21 +7,6 @@ import {Component} from '@angular/core';
 })
 export class HomeComponent {
 
-  name = "internshala training";
-  husbandName = "Mark Zuckerberg";
-  wifeName = "Priscilla Chan";
-
-  people: any = [
-    {
-      'name': 'Mark Zuckerberg',
-      'gender': 'male'
-    },
-    {
-      'name': 'Priscilla Chan',
-      'gender': 'female'
-    }
-  ]
-
   constructor() {
   }
 
